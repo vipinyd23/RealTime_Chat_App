f51bcc2c336f0543271d00ecd5ba264ae0f3e768
vipinyd23 <vipinyd46@gmail.com>
2023-10-03 22:46:48 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express=require('express')
+const app=express()
+const http=require('http').createServer(app)
+const PORT=process.env.PORT ||3000
+
+
+app.use(express.static(__dirname+'/public'))
+http.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`)
+})
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+"/index.html")
+})
+//socket
+const io=require("socket.io")(http)
+
+io.on("connection",(socket)=>{
+    console.log('connected')
+    socket.on('message',(msg)=>{
+        socket.broadcast.emit('message',msg)
+    })
+})
